e509a2d5d48ddab07c520055ad0a7178c147ad47
ATOU11 <prince.atou@epitech.digital>
2024-09-24 15:46:30 +0200

    Update ex_04.js

--- ex_04.js
+++ ex_04.js
@@ -3,7 +3,7 @@ HTML:
 Welcome to the javascript pool! <br> Have some fun and dont forget to bring a towel.
 </h3>
 partieJS:
-console.log(« Website launched! »)
+console.log(« Website launched! »);
 
 
 
